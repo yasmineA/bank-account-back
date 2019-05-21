91b82890f3cd0cbd1ca64498bd9e42109cad76d4
yass <amarayassmine@gmail.com>
2019-05-21 15:39:46 +0200

    fix test

--- src/test/java/com/sg/bankaccountback/AccountTest.java
+++ src/test/java/com/sg/bankaccountback/AccountTest.java
@@ -24,40 +24,39 @@ public class AccountTest {
     @Autowired
     AccountService accountService;
 
-    Account account = new Account(1, "MM Y A", "30003 01199 11150000182 66", 100,Status.ACTIVE, new Date());
-    Account accountB = new Account(2, "MM Y A", "30003 01199 11150000182 66", 100,Status.BLOCKED, new Date());
-    Account accountC = new Account(3, "MM Y A", "30003 01199 11150000182 66", 100,Status.CLOSED, new Date());
+    Account accountB = new Account(1, "MM Y A", "30003 01199 11150000182 66", 100,Status.BLOCKED, new Date());
+    Account account = new Account(2, "MM Y A", "30003 01199 11150000182 66", 100,Status.ACTIVE, new Date());
 
 
 
     @Test
     public void testDepositAmount(){
-        float initialBalance = account.getBalance();
+        float initialBalance = accountService.history(account.getId()).getBalance();
         float amount = 10;
-        accountService.deposit(1, amount);
-        assertEquals((amount+initialBalance),account.getBalance(), 0.0d);
+        accountService.deposit(account.getId(), amount);
+        assertEquals((amount+initialBalance), accountService.history(account.getId()).getBalance(), 0.0d);
     }
 
     @Test
     public void testDepositNegativeAmount(){
         try {
-            accountService.deposit(1, -1000);
+            accountService.deposit(accountB.getId(), -1000);
         } catch (BadRequestException e) {
         }
     }
 
     @Test
     public void testWithdrawal(){
-        float initialBalance = account.getBalance();
+        float initialBalance = accountService.history(account.getId()).getBalance();
         float amount = 10;
-        accountService.withdraw(1, 10, Operation.WITHDRAWAL);
-        assertEquals((initialBalance-amount),account.getBalance(), 0.0d);
+        accountService.withdraw(account.getId(), 10, Operation.WITHDRAWAL);
+        assertEquals((initialBalance-amount), accountService.history(account.getId()).getBalance(), 0.0d);
     }
 
     @Test
     public void testWithdrawHighAmount(){
         try {
-            accountService.withdraw(1,1000, Operation.WITHDRAWAL);
+            accountService.withdraw(account.getId(),1000, Operation.WITHDRAWAL);
         } catch (BadRequestException e) {
         }
     }
@@ -65,15 +64,7 @@ public class AccountTest {
     @Test
     public void testWithdrawalBlockedAcc(){
         try {
-            accountService.withdraw(2, 10, Operation.WITHDRAWAL);
-        } catch (NotAllowedException e) {
-        }
-    }
-
-    @Test
-    public void testWithdrawalClosedAcc(){
-        try {
-            accountService.withdraw(3,10, Operation.WITHDRAWAL);
+            accountService.withdraw(accountB.getId(), 10, Operation.WITHDRAWAL);
         } catch (NotAllowedException e) {
         }
     }
@@ -89,7 +80,7 @@ public class AccountTest {
     @Test
     public void testGetHistory(){
         try {
-            assertNull(accountService.history(1));
+            assertEquals(accountService.history(account.getId()).getStatus(),accountService.history(account.getId()).getStatus());
         } catch (NotFoundException e) {
         }
     }
